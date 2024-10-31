aa708e44f4195c2eecb2287bdc532e8a485e8562
FireCubeStudios <40955345+FireCubeStudios@users.noreply.github.com>
2024-10-31 14:40:08 +0100

    Switch to razor pages

--- devsanx/Program.cs
+++ devsanx/Program.cs
@@ -1,36 +1,34 @@
-using devsanx.Components;
-
 namespace devsanx
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddRazorComponents()
-                .AddInteractiveServerComponents();
+			// Add services to the container.
+			builder.Services.AddRazorPages();
 
-            var app = builder.Build();
+			var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+			// Configure the HTTP request pipeline.
+			if (!app.Environment.IsDevelopment())
+			{
+				app.UseExceptionHandler("/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
 
-            app.UseHttpsRedirection();
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
 
-            app.UseStaticFiles();
-            app.UseAntiforgery();
+			app.UseRouting();
 
-            app.MapRazorComponents<App>()
-                .AddInteractiveServerRenderMode();
+			app.UseAuthorization();
 
-            app.Run();
-        }
-    }
+			app.MapRazorPages();
+
+			app.Run();
+		}
+	}
 }
